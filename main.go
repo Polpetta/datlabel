c97ad3d06f0a2f8a27b0d68351d1ec1543357612
Davide Polonio <poloniodavide@gmail.com>
2019-07-15 23:38:17 +0200

    First commit. Not working

---
+++ main.go
@@ -0,0 +1,44 @@
+package dockerlabel
+
+import (
+	"context"
+
+	//"github.com/docker/docker/api/types"
+	docker "github.com/docker/docker/client"
+)
+
+func newDockerClient() *docker.Client {
+	cli, err := docker.NewClientWithOpts(docker.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	return cli
+}
+
+func GetLabelsFromContainer(containerId string) []string {
+	result := []string{}
+	cli := newDockerClient()
+	containerDetails, err := cli.ContainerInspect(context.Background(),
+		containerId)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for key, value := range containerDetails.Config.Labels {
+		if value != "false" {
+			result = append(result, key)
+		}
+	}
+
+	return result
+}
+
+func GetLabelsFromService(serviceId string) []string {
+	return []string{}
+}
+
+func GetLabelsFromStack(stackId string) []string {
+	return []string{}
+}
